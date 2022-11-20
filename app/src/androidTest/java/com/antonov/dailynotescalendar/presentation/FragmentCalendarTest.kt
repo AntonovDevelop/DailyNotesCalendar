35214fdec383f92b68b38fa594d39a775b0d9a1e
AntonovDevelop <antonov04092002@mail.ru>
2022-11-20 12:56:05 +0400

    version 1.18 need to add ui tests

--- app/src/androidTest/java/com/antonov/dailynotescalendar/presentation/FragmentCalendarTest.kt
+++ app/src/androidTest/java/com/antonov/dailynotescalendar/presentation/FragmentCalendarTest.kt
@@ -1,19 +1,16 @@
 package com.antonov.dailynotescalendar.presentation
 
-import androidx.core.content.MimeTypeFilter.matches
 import androidx.fragment.app.testing.FragmentScenario
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.antonov.dailynotescalendar.presentation.mainFragments.CalendarFragment
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import com.antonov.dailynotescalendar.R
 
+@HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
 class FragmentCalendarTest {
     private lateinit var scenario: FragmentScenario<CalendarFragment>
--- app/src/test/java/com/antonov/dailynotescalendar/data/room/repository/RoomNotesRepositoryTest.kt
+++ app/src/test/java/com/antonov/dailynotescalendar/data/room/repository/RoomNotesRepositoryTest.kt
@@ -1,25 +1,10 @@
 package com.antonov.dailynotescalendar.data.room.repository
 
-import com.antonov.dailynotescalendar.domain.model.Note
-import com.antonov.dailynotescalendar.domain.repository.INotesRepository
 import org.junit.Assert.*
+import org.mockito.Mock
 
-class FakeNotesRepositoryTest: INotesRepository{
-    private val repository = mutableListOf<Note>()
-    override suspend fun insertAllItems(items: List<Note>) {
-        repository.addAll(items)
-    }
-
-    override suspend fun insert(item: Note) {
-        repository.add(item)
-    }
-
-    override suspend fun getAllItems(): List<Note> {
-        return repository
-    }
-
-    override suspend fun deleteItem(item: Note) {
-        repository.remove(item)
-    }
+class RoomNotesRepositoryTest{
+    @Mock
+    lateinit var roomNotesRepository: RoomNotesRepository
 
 }
